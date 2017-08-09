9e8023edad63d60a94fbcf4f22cf8eaee6bb7d7f
MiriamPeskowitz <MiriamPeskowitz@users.noreply.github.com>
2017-08-09 15:01:15 -0400

    python code for motion sensor email send
    Girls + Tech book, chapter 5

--- code.py
+++ code.py
@@ -5,24 +5,24 @@ import smtplib
 m = MotionSensor(4)
 
 def emailSend():
-  senderEmail = "yourEmail@gmail.com"
-  password = "yourPassword"
-  receiver = "girlsandtechbook@gmail.com"
-  message = "Subject: Someone's in your room!"
-  server = smtplib.SMTP('smtp.gmail.com', 587)
-  server.starttls()
-  server.ehlo()
-  server.login(senderEmail, password)
-  print("Email sent")
+      senderEmail = "yourEmail@gmail.com"
+      password = "yourPassword"
+      receiver = "girlsandtechbook@gmail.com"
+      message = "Subject: Someone's in your room!"
+      server = smtplib.SMTP('smtp.gmail.com', 587)
+      server.starttls()
+      server.ehlo()
+      server.login(senderEmail, password)
+      print("Email sent")
   
 while True:
-  if m.motion_detected: 
-    emailSend()
-    print("Who goes there?")
-    sleep(2)
-  else: 
-    print("All quiet")
-    sleep(2)
-    
+      if m.motion_detected: 
+            emailSend()
+            print("Who goes there?")
+            sleep(2)
+      else: 
+            print("All quiet")
+            sleep(2)
+
     
     
